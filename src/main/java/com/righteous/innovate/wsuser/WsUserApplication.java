b6cfbfffaa65178cd8d7f368b67925bf1e99d75e
For404Codes <zrj2214537@163.com>
2024-01-22 22:03:49 +0800

    大致框架（未测试，未添加数据库）

---
+++ src/main/java/com/righteous/innovate/wsuser/WsUserApplication.java
@@ -0,0 +1,13 @@
+package com.righteous.innovate.wsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/righteous/innovate/wsuser/WsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.righteous.innovate.wsuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
